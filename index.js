4906203a60b79a9385bdf5228bd60365bea1db20
Kunj Patel <pkunjk007@gmail.com>
2025-08-28 08:19:22 -0600

    First commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const express= require('express')
+const app = express()
+app.use(express.json())
+let notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: "2",
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
+
+app.get('/',( request, response)=>{
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/note', (request,response)=>{
+  response.json(notes)
+})
+ 
+app.get('/api/note/:id',(request, response)=>{
+  const id= request.params.id
+  const note= notes.find(note=>note.id===id)
+  if (note){
+    response.json(note)
+  }
+  else{
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/note/:id', (request, response)=>{
+  const id= request.params.id
+  const note= notes.filter(note=>note.id!==id)
+  response.status(204).end()
+})
+
+app.post('/api/note', (request, response)=>{
+  const note= request.body
+  console.log(note)
+  response.json()
+})
+
+const PORT= 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
