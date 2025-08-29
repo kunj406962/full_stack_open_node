710bc7ac58f041ba9cd239705a7106a11368b971
Kunj Patel <pkunjk007@gmail.com>
2025-08-28 20:01:20 -0600

    re worked the post

--- index.js
+++ index.js
@@ -44,11 +44,33 @@ app.delete('/api/note/:id', (request, response)=>{
   response.status(204).end()
 })
 
-app.post('/api/note', (request, response)=>{
-  const note= request.body
-  console.log(note)
-  response.json()
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => Number(n.id)))
+    : 0
+  return String(maxId + 1)
+}
+
+app.post('/api/note', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
 })
+
 
 const PORT= 3001
 app.listen(PORT)
